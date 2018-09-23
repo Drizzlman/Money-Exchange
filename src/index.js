b4ecac3c9dfa3044bca2f34725a75cd839df779f
Drizzlman <kovalko.alexey@yandex.by>
2018-09-23 13:26:38 +0300

    Task_5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency >10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    if(currency<=0){
+        return{};
+    }
+    var Answear = {};
+    if (currency>= 50){
+        Answear.H = parseInt(currency/50)|| undefined;
+        currency = currency % 50;
+    }
+    if (currency>= 25){
+        Answear.Q = parseInt(currency/25)|| undefined;
+        currency = currency % 25;
+    }
+    if (currency>= 10){
+        Answear.D = parseInt(currency/10)|| undefined;
+        currency = currency % 10;
+    }
+    if (currency>= 5){
+        Answear.N = parseInt(currency/5)|| undefined;
+        currency = currency % 5;
+    }
+    if (currency>= 1){
+        Answear.P = currency;
+    }
+    return Answear;
 }
